d139951f3d933e84b3034964b40c5c1392c573a7
Lachezar <uzunov_89_lacho@abv.bg>
2023-06-29 15:03:46 +0300

    inner pages mobile optimization

--- components/paintings/painting.js
+++ components/paintings/painting.js
@@ -5,17 +5,17 @@ export default function Painting({ src, alt, technique, width, height, dimension
 
     return (
         <div className={classes.image_section}>
-            <div className={classes.display}>
+            <div className={`${classes.display}`}>
                 <Image src={src} alt={alt} width={width * 1.2} height={height * 1.2}/>
             </div>
-            <div className={classes.display_sm}>
+            <div className={`${classes.display_sm} ${classes.centered}`}>
                 <Image src={src} alt={alt} width={width} height={height}/>
             </div>
             <div>
-                <h2>{alt}</h2>
+                <h2 className={classes.title}>{alt}</h2>
                 <p>Техника на рисуване - {technique}</p>
                 <p>Размери в сантиметри - {dimensions}</p>
-                <p>{text}</p>
+                <p className={classes.text}>{text}</p>
             </div>  
         </div>
     )
