debf90f664e879c6fabdb155c678c074d05ce465
Lachezar <uzunov_89_lacho@abv.bg>
2023-08-09 16:22:36 +0300

    escaping quotes and apos

--- components/home-page/about-me.js
+++ components/home-page/about-me.js
@@ -17,32 +17,32 @@ const AboutMe = ({ bulgarian }) => {
         <section className={classes.info}>
           <article className={classes.display}>
             {bulgarian ?
-              <h4>{`О, вий, които имате очи печални, с безкрая синкав на морето ги хранете`}</h4>
+              <h4>О, вий, които имате очи печални, с безкрая синкав на морето ги хранете</h4>
             :
-              <h4 className={classes.henni}>{`Oh ye! who have your eye-balls vex'd and tir'd, Feast them upon the wideness of the Sea`}</h4>
+              <h4 className={classes.henni}>Oh ye! who have your eye-balls vex&apos;d and tir&apos;d, Feast them upon the wideness of the Sea</h4>
             }
            
           </article>
           <article>
             { bulgarian ? 
-              <p className={`shadow`}>{`Родена съм през ноември 1989г., един мил скорпион. 😉`}</p>
+              <p className={`shadow`}>Родена съм през ноември 1989г., един мил скорпион. 😉</p>
             :
-              <p className={`${classes.henni} shadow`}>{`I was born November , 1989., a kind scorpio. 😉`}</p>
+              <p className={`${classes.henni} shadow`}>I was born November , 1989., a kind scorpio. 😉</p>
             }
 
             { bulgarian ? 
-              <p className={`shadow`}>{`През 2008 г. завърших ЕГ ,,Иван Вазов‘‘ гр. Смолян с немски  и английски език. Същата година продължих образованието си в СУ ,,Св. Климент Охридски‘‘ , специалност Немска филология, а впоследствие записах магистратура ,,Философия на съзнанието на езика‘‘. През 2022 г. се дипломирах в магистърската програма на НБУ ,,Криминология и политики за превенция на престъпността.‘‘`}</p>
+              <p className={`shadow`}>През 2008 г. завърших ЕГ &quot;Иван Вазов&quot; гр. Смолян с немски  и английски език. Същата година продължих образованието си в СУ &quot;Св. Климент Охридски&quot; , специалност Немска филология, а впоследствие записах магистратура &quot;Философия на съзнанието на езика&quot;. През 2022 г. се дипломирах в магистърската програма на НБУ &quot;Криминология и политики за превенция на престъпността.&quot;</p>
             :
-              <p className={`${classes.henni} shadow`}>{`In 2008, I graduated from Language School "Ivan Vazov" in Smolyan with German and English. The same year, I continued my education at Sofia University "St. Kliment Ohridski", majoring in German philology. I subsequently enrolled in a Master's degree in "Philosophy of Mind and Language". In 2022, I graduated from the Master's program of the NBU, "Criminology and Crime Prevention Policies."`}</p>
+              <p className={`${classes.henni} shadow`}>In 2008, I graduated from Language School &quot;Ivan Vazov&quot; in Smolyan with German and English. The same year, I continued my education at Sofia University &quot;St. Kliment Ohridski&quot;, majoring in German philology. I subsequently enrolled in a Master&apos;s degree in &quot;Philosophy of Mind and Language&quot;. In 2022, I graduated from the Master&apos;s program of the NBU, &quot;Criminology and Crime Prevention Policies.&quot;</p>
             }
            
             { bulgarian ? 
               <h4 className={classes.display_m}>
-                Щастлив е този, който може да види красотата в обичайните неща, там, където другите не виждат нищо!
+                О, вий, които имате очи печални, с безкрая синкав на морето ги хранете
               </h4>
             :
               <h4 className={`${classes.display_m} ${classes.henni}`}>
-                Blessed are they who see beautiful things in humble places where other people see nothing.
+                Oh ye! who have your eye-balls vex&apos;d and tir&apos;d, Feast them upon the wideness of the Sea
               </h4>
             }
 
