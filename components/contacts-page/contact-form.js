aa2e35b47a83500c2c9207255ad9d74117813a85
Lachezar Uzunov <uzunov_89_lacho@abv.bg>
2023-10-26 11:02:59 +0300

    add email service

--- components/contacts-page/contact-form.js
+++ components/contacts-page/contact-form.js
@@ -1,11 +1,37 @@
 import LanguageContext from "@/store/language";
 import classes from "./contact-form.module.css";
-import { useContext } from "react";
+import { useContext, useRef, useState } from "react";
+import emailjs from 'emailjs-com';
 
 const ContactForm = () => {
+  const form = useRef();
+  const [sent, setSent] = useState(false);
   const languageCtx = useContext(LanguageContext);
 
   const bulgarian = languageCtx.version;
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log(e);
+    emailjs.sendForm(
+      "service_vwdlf88",
+      "template_fd1v9lk",
+      form.current,
+      "Nx0o_yOvAxqAluK_u"
+    )
+    .then(
+      (result) => {
+        console.log(result.text);
+        e.target.reset();
+        setSent(true);
+        setTimeout(() => {
+          setSent(false);
+        }, 7000);
+      },
+      (error) => {
+        console.log(error.text);
+      })
+  }
 
   return (
     <section className={classes.form__section}>
@@ -21,28 +47,43 @@ const ContactForm = () => {
         :
           <h2 className={`${classes.header_two} henni`}>Drop me a line</h2>
         }
-        
+
+{sent && bulgarian ? (
+            <h2 className={`${classes.header_two} bgFont`}>
+              Вашето запитване беше изпратено успешно. Ще се свържа с Вас във възможно най-кратък срок.
+            </h2>
+          ) : null}
+        {sent && !bulgarian ? (
+        <h2 className={`${classes.header} typewriter`}>
+          Your message was sent successfully. I will get back to you as soon
+          as possible.
+        </h2>
+      ) : null}
       </div>
       <div className={classes.form_center}>
-        <form className={classes.form}>
+        <form 
+          className={classes.form} 
+          ref={form}
+          onSubmit={sendEmail}
+        >
           { bulgarian ?
             <label className="bgFont" htmlFor="name">Име</label>
           :
             <label className="henni" htmlFor="name">Name</label>
           }
-          <input id="name" required />
+          <input id="name" name="name" type="text" required />
           { bulgarian ?
             <label className="bgFont" htmlFor="name">Имейл</label>
           :
             <label className="henni" htmlFor="name">Email</label>
           }
-          <input id="email" required />
+          <input id="email" name="email" type="email" required />
           { bulgarian ?
             <label className="bgFont" htmlFor="name">Вашето запитване</label>
           :
             <label className="henni" htmlFor="name">Your message</label>
           }
-          <textarea required id="question"></textarea>
+          <textarea required id="question" name="message" type="text"></textarea>
           <div className={classes.centered_btn}>
             {bulgarian ?
               <button className={`btn__primary bgFont`}>Изпрати</button>
